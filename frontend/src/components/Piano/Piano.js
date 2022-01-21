f2be2b5ff5a4dde22a00d1bc5f5a6e138644edd1
Valeria Starkova <stavaser@icloud.com>
2022-01-20 20:15:04 -0500

    added hints

--- frontend/src/components/Piano/Piano.js
+++ frontend/src/components/Piano/Piano.js
@@ -1,74 +1,114 @@
-import React from 'react';
-import { StyledPiano } from './Piano.styled';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { StyledPiano, Key } from './Piano.styled';
 const notes = [
   {
+    id: 0,
     name: 'C',
     color: 'white',
   },
   {
+    id: 1,
     name: 'C#',
     color: 'black',
   },
   {
+    id: 2,
     name: 'D',
     color: 'white',
   },
   {
+    id: 3,
     name: 'D#',
     color: 'black',
   },
   {
+    id: 4,
     name: 'E',
     color: 'white',
   },
   {
+    id: 5,
     name: 'F',
     color: 'white',
   },
   {
+    id: 6,
     name: 'F#',
     color: 'black',
   },
   {
+    id: 7,
     name: 'G',
     color: 'white',
   },
   {
+    id: 8,
     name: 'G#',
     color: 'black',
   },
   {
+    id: 9,
     name: 'A',
     color: 'white',
   },
   {
+    id: 10,
     name: 'A#',
     color: 'black',
   },
   {
+    id: 11,
     name: 'B',
+    color: 'white',
+  },
+  {
+    id: 12,
+    name: 'C',
     color: 'white',
   },
 ];
 const Piano = ({ input }) => {
-  // console.log(input);
+  const [hint, setHint] = useState(false);
+  const state = useSelector((state) => state);
+
+  const showHint = () => {
+    setHint(true);
+    setTimeout(() => {
+      setHint(false);
+    }, 300);
+    setTimeout(() => {
+      setHint(true);
+    }, 600);
+    setTimeout(() => {
+      setHint(false);
+    }, 900);
+    setTimeout(() => {
+      setHint(true);
+    }, 1200);
+    setTimeout(() => {
+      setHint(false);
+    }, 1500);
+  };
+
+  console.log(hint);
   return (
     <StyledPiano>
       <div className="container">
-        <div className="keyandnote">
-          <div className="keyboard">
-            {notes.map((note) => {
-              return (
-                <button
-                  key={note.name}
-                  className={note.color}
-                  id={input == note.name ? 'active' : ''}
-                >
-                  {note.name}
-                </button>
-              );
-            })}
-          </div>
+        <a onClick={() => showHint()}>Show hint</a>
+
+        <div>
+          {notes.map((note) => {
+            return (
+              <Key
+                key={note.id}
+                isCorrect={hint && state.notes.includes(note.name)}
+                className={`${note.color} ${input == note.name ? 'active' : ''}`}
+              >
+                <span>{note.name}</span>
+              </Key>
+            );
+          })}
         </div>
       </div>
     </StyledPiano>
--- frontend/src/components/Piano/Piano.styled.js
+++ frontend/src/components/Piano/Piano.styled.js
@@ -3,56 +3,78 @@ import styled from 'styled-components';
 export const StyledPiano = styled.div`
   display: flex;
   justify-content: center;
-  height: 100%;
 
-  .keyandnote {
+  .container {
     align-self: center;
+    background: orange;
+    border-radius: 20px;
+    padding: 40px;
+    padding-top: 20px;
+  }
+  .container a {
+    display: block;
+    margin-bottom: 20px;
+    font-weight: bold;
+    color: rgba(255, 255, 255, 0.8);
+    cursor: pointer;
+  }
+  .container a:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Key = styled.button`
+  @keyframes twinkle_white {
+    from {
+      background: #f7fff7;
+    }
+    to {
+      background: #a1eda1;
+    }
   }
 
-  .keyboard {
-    font-size: 12px;
-    font-weight: 400;
-    display: inline-table;
-    padding: 24px;
-    border-radius: 20px 20px 40px 40px;
-    background: orange;
-  }
-  .keyboard button {
-    outline: none;
-    transition: all 100ms ease-out;
-    margin: 0px;
-    padding: 0px;
-    border: none;
-    color: rgba(26, 83, 92, 0.5);
-    border-radius: 20px 20px 40px 40px;
-    cursor: pointer;
-    font-weight: bold;
-  }
-  .keyboard button.white {
-    background: #f7fff7;
+  outline: none;
+  border: none;
+  margin: 0px;
+  padding: 0px;
+  color: rgba(26, 83, 92, 0.5);
+  border-radius: 10px 10px 20px 20px;
+  cursor: pointer;
+  font-size: 30px;
+  font-weight: bold;
+  transition: 100ms ease-out;
+
+  &.white {
+    background: ${({ isCorrect }) => (isCorrect ? '#a1eda1' : '#f7fff7')};
     width: 80px;
     height: 280px;
-    box-shadow: inset #e6f1e6 0 -10px 0 0;
+    box-shadow: inset ${({ isCorrect }) => (isCorrect ? '#75bf75' : '#e6f1e6')} 0 -16px 0 0;
     padding-top: 160px;
     margin: 1px;
   }
-  .keyboard button.white#active {
-    box-shadow: inset #e6f1e6 0 -2px 0 0;
-    transform: translate(0, 4px);
+  &.white.active {
+    box-shadow: inset ${({ isCorrect }) => (isCorrect ? '#75bf75' : '#e6f1e6')} 0 -8px 0 0;
+    transform: translateY(5px);
     height: -8px;
   }
-  .keyboard button.black {
-    background: #1a535c;
+  &.black {
+    background: ${({ isCorrect }) => (isCorrect ? '#78c478' : '#4e2878')};
     width: 64px;
     height: 180px;
     z-index: 99;
     position: absolute;
-    transform: translate(-32px, -20px);
-    box-shadow: inset #073b4c 0 -10px 0 0, var(--keyboard-mode) 0 0 0 2px;
+    transform: translate(-32px, -8px);
+    box-shadow: inset ${({ isCorrect }) => (isCorrect ? '#579457' : '#2b0e4a')} 0 -16px 0 0;
   }
-  .keyboard button.black#active {
+  &.black span {
+    position: absolute;
+    transform: translate(-18px, -20px);
+    color: rgba(255, 255, 255, 0.5);
+    bottom: 0;
+  }
+  &.black.active {
     transform: translate(-32px, -4px);
     height: -8px;
-    box-shadow: inset #073b4c 0 -2px 0 0, var(--keyboard-mode) 0 0 0 2px;
+    box-shadow: inset ${({ isCorrect }) => (isCorrect ? '#579457' : '#2b0e4a')} 0 -8px 0 0;
   }
 `;
